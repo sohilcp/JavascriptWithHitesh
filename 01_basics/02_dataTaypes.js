0b32face54283ba82a52b1c6f248029db30ece2f
sohilcp <131880263+sohilcp@users.noreply.github.com>
2023-07-29 23:26:57 +0000

    dataTypes

---
+++ 01_basics/02_dataTaypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as Newer Version
+
+//alert(3+3) we are using node js not browser!
+
+let name = "Sohil"//string datatype
+let age = 32 // number datatype
+let isloggedIn = true // boolean datatype
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolen => true/flase
+//null => standalon value it´s also object 
+// undefined =>
+//symbol => uinque
+
+//object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object 
