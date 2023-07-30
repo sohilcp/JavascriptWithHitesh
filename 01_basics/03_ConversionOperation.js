d0021a29ed688fce87c86e15ac940ec5e3de5e57
sohilcp <131880263+sohilcp@users.noreply.github.com>
2023-07-30 19:39:31 +0000

    Conversion Of datatype

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,29 @@
+let score = true;
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1 ; false => 0
+
+let isLoggedIn = "sohil";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// "" => flase
+//"sohil" => true
+
+let someNumber = 18
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
