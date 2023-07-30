98cd08191b614f01640b8e868fece2c0d8eb566d
sohilcp <131880263+sohilcp@users.noreply.github.com>
2023-07-30 20:11:34 +0000

    add some Operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -27,3 +27,33 @@ let stringNumber = String(someNumber);
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// «««««««««««« Operations »»»»»»»»»»»»»
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "Hello"
+let str2 = " Sohil"
+let srt3 = str1 + str2
+
+console.log(srt3);
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2 );//122
+console.log(1 + 2 + "2");// 32
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+gameCounter--
+console.log(gameCounter);
